6964eaf50730af7b3f3126654e80e1bb6d1ccacf
João Vitor <jv17fc@gmail.com>
2020-06-12 22:11:09 -0300

    Refatorando código

--- app.js
+++ app.js
@@ -5,29 +5,38 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiUrl = `https://api.lyrics.ovh`
 
+// criando requisição padrão
+const fetchData = async url => {
+    const response = await fetch(url)
+    return await response.json()
+}
+
 // cors
 const getMoreSongs = async url => {
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
-    const data = await response.json()
-
+    const data = await fetchData(`https://cors-anywhere.herokuapp.com/${url}`)
     insertSongsIntoPage(data)
 }
 
+// inserir botões de next e prev
+const insertNextAndPrevButtons = ({ prev, next }) => {
+    prevAndNextContainer.innerHTML = `
+    ${prev ? `<button class="btn" onclick="getMoreSongs('${prev}')">Anteriores</button>` : '' }
+    ${next ? `<button class="btn" onclick="getMoreSongs('${next}')">Próximas</button>` : '' }
+`
+}
+
 // inserir lis na tela de acordo com a resposta
-const insertSongsIntoPage = songsInfo => {
-    songsContainer.innerHTML = songsInfo.data.map(song => `
+const insertSongsIntoPage = ({ data, prev, next }) => {
+    songsContainer.innerHTML = data.map(({ artist: { name }, title }) => `
         <li class="song">
-            <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
-            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+            <span class="song-artist"><strong>${name}</strong> - ${title}</span>
+            <button class="btn" data-artist="${name}" data-song-title="${title}">Ver letra</button>
         </li>
     `).join('')
 
     // botão prev e next
-    if (songsInfo.prev || songsInfo.next) {
-        prevAndNextContainer.innerHTML = `
-            ${songsInfo.prev ? `<button class="btn" onclick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : '' }
-            ${songsInfo.next ? `<button class="btn" onclick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : '' }
-        `
+    if (prev || next) {
+        insertNextAndPrevButtons({ prev, next })
         return
     }
 
@@ -36,18 +45,17 @@ const insertSongsIntoPage = songsInfo => {
 
 // requisição de acordo com o que foi digitado no input
 const fetchSongs = async term => {
-    const response = await fetch(`${apiUrl}/suggest/${term}`)
-    const data = await response.json()
-
+    const data = await fetchData(`${apiUrl}/suggest/${term}`)
     insertSongsIntoPage(data)
 }
 
-// ação do botão buscar
-form.addEventListener('submit', event => {
+const hendleFormSubmit = event => {
     event.preventDefault()
 
     // pegando valor digitado e removendo espaços vazios
     const searchTerm = searchInput.value.trim()
+    searchInput.value = ''
+    searchInput.focus()
 
     // se tiver vazio a busca retorna uma mensagem
     if (!searchTerm) {
@@ -57,14 +65,13 @@ form.addEventListener('submit', event => {
 
     // se tiver com valor o input passa pra função fetchSongs o que o usuário digitou
     fetchSongs(searchTerm)
-})
+}
 
-// pegando letra da música e exibindo na tela
-const fetchLyrics = async (artist, songTitle) => {
-    const response = await fetch(`${apiUrl}/v1/${artist}/${songTitle}`)
-    const data = await response.json()
-    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+// ação do botão buscar
+form.addEventListener('submit', hendleFormSubmit)
 
+// inserir informações na página de ver letra
+const insertLyricsIntoPage = ({ lyrics, artist, songTitle })  => {
     songsContainer.innerHTML = `
         <li class="lyrics-container">
             <h2><strong>${songTitle}</strong> - ${artist}</h2>
@@ -73,8 +80,15 @@ const fetchLyrics = async (artist, songTitle) => {
     `
 }
 
-// pegar música clicada
-songsContainer.addEventListener('click', event => {
+// pegando letra da música e exibindo na tela
+const fetchLyrics = async (artist, songTitle) => {
+    const data = await fetchData(`${apiUrl}/v1/${artist}/${songTitle}`)
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+    insertLyricsIntoPage({ lyrics, artist, songTitle })
+}
+
+// pegar musica clicada
+const hendleSongsContainerClick = event => {
     const clickedElement = event.target
 
     if (clickedElement.tagName === 'BUTTON') {
@@ -86,4 +100,7 @@ songsContainer.addEventListener('click', event => {
 
         fetchLyrics(artist, songTitle)
     }
-})
+}
+
+// evento de click
+songsContainer.addEventListener('click', hendleSongsContainerClick)
